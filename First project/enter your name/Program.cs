87336790e50d31d9c97aa10683643f4e9696cd50
Parhamnayeri <parham.ny.shiraz@gmail.com>
2023-05-26 01:19:24 +0430

    update

---
+++ First project/enter your name/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace enter_your_name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("PLS ENTER YOUR NAME");
+            name = Console.ReadLine();
+            Console.WriteLine("WHATSUP\t" + name);
+            Console.WriteLine("PRESS ANY KEY TO EXIT");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ First project/mult3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace mult3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("enter a number to get mult*3");
+            string number = Console.ReadLine();
+            a = int.Parse(number);
+            a = 3 * a;
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
